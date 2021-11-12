730feb6dd3b0c3b662ab935f5680db92f06fcf8b
Rayapu-Mahesh <92463419+Rayapu-Mahesh@users.noreply.github.com>
2021-11-12 17:30:22 +0530

    Add files via upload

---
+++ SQLQuery_tabularfunction_rangeyear.sql
@@ -0,0 +1,29 @@
+
+
+CREATE FUNCTION orders_in_range (
+    @start_year datetime,
+    @end_year datetime	
+)
+RETURNS TABLE
+AS
+RETURN
+    SELECT 
+        order_id,
+       customer_id,
+        order_status,
+		store_id
+		
+    FROM
+        sales_orders
+    WHERE
+        year(order_date) BETWEEN @start_year AND @end_year
+
+
+		
+SELECT 
+    order_id,
+       customer_id,
+        order_status,
+		store_id
+FROM 
+    orders_in_range(2016,2017)
